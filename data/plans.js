a6ca9e0fb0310caa44fa1fa339a9f908bb2d7b7b
DJ-MrJay <djmrjay@gmail.com>
2024-02-29 16:19:25 +0300

    Implement plans screen UI

---
+++ data/plans.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    id: "0",
+    name: "Mobile",
+    price: 200,
+    videoQuality: "Fair",
+    resolution: "480p",
+    numberOfDevices: 1,
+    downloadDevices: 1,
+    devices: [
+      {
+        id: "10",
+        name: "mobile",
+      },
+      {
+        id: "11",
+        name: "tablet",
+      },
+    ],
+  },
+  {
+    id: "100",
+    name: "Basic",
+    price: 300,
+    videoQuality: "Good",
+    resolution: "720p (HD)",
+    numberOfDevices: 1,
+    downloadDevices: 1,
+    devices: [
+      {
+        id: "20",
+        name: "mobile",
+      },
+      {
+        id: "21",
+        name: "tablet",
+      },
+      {
+        id: "25",
+        name: "laptop",
+      },
+      {
+        id: "45",
+        name: "tv",
+      },
+    ],
+  },
+  {
+    id: "2",
+    name: "Standard",
+    price: 700,
+    videoQuality: "Better",
+    resolution: "1080p (Full HD)",
+    numberOfDevices: 2,
+    downloadDevices: 2,
+    devices: [
+      {
+        id: "20",
+        name: "mobile",
+      },
+      {
+        id: "21",
+        name: "tablet",
+      },
+      {
+        id: "25",
+        name: "laptop",
+      },
+      {
+        id: "45",
+        name: "tv",
+      },
+    ],
+  },
+  {
+    id: "3",
+    name: "Premium",
+    price: 1100,
+    videoQuality: "Best",
+    resolution: "4K (Ultra HD) + HDR",
+    numberOfDevices: 4,
+    downloadDevices: 6,
+    devices: [
+      {
+        id: "20",
+        name: "mobile",
+      },
+      {
+        id: "21",
+        name: "tablet",
+      },
+      {
+        id: "25",
+        name: "laptop",
+      },
+      {
+        id: "45",
+        name: "tv",
+      },
+    ],
+  },
+];
--- screens/Plans.js
+++ screens/Plans.js
@@ -1,14 +1,179 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Pressable, StyleSheet, Text, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React from "react";
+import { AntDesign } from "@expo/vector-icons";
+import plans from "../data/plans";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 const Plans = () => {
+  const data = plans;
   return (
-    <View>
-      <Text>Plans</Text>
-    </View>
-  )
-}
+    <ScrollView style={{ marginTop: 50 }}>
+      <View style={{ padding: 20 }}>
+        <Text
+          style={{
+            fontSize: 30,
+            fontWeight: "600",
+            marginBottom: 16,
+          }}
+        >
+          Choose the plan that's right for you
+        </Text>
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+          <AntDesign name="check" size={20} color="#e50914" />
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "400",
+            }}
+          >
+            No commitments, cancel anytime.
+          </Text>
+        </View>
 
-export default Plans
+        <View style={{ flexDirection: "row", alignItems: "center", gap: 5 }}>
+          <AntDesign name="check" size={20} color="#e50914" />
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "400",
+            }}
+          >
+            Everything on Netflix for one low price.
+          </Text>
+        </View>
 
-const styles = StyleSheet.create({})
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            gap: 5,
+            marginBottom: 16,
+          }}
+        >
+          <AntDesign name="check" size={20} color="#e50914" />
+          <Text
+            style={{
+              fontSize: 17,
+              fontWeight: "400",
+            }}
+          >
+            No ads and no extra fees. Ever.
+          </Text>
+        </View>
+
+        {data.map((item, index) => (
+          <Pressable
+            style={{
+              borderRadius: 5,
+              borderColor: "#e50914",
+              borderWidth: 1,
+              padding: 15,
+              marginBottom: 16,
+            }}
+            key={index}
+          >
+            <View
+              style={{
+                flexDirection: "row",
+                alignItems: "flex-start",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "#e50914",
+                  padding: 10,
+                  width: 120,
+                  borderRadius: 5,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    color: "white",
+                    fontWeight: "600",
+                  }}
+                >
+                  {item.name}
+                </Text>
+              </View>
+              <Text style={{ fontWeight: "600" }}>
+                Monthly price: Ksh {item.price.toLocaleString()}
+              </Text>
+            </View>
+
+            <View
+              style={{
+                marginTop: 15,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <View>
+                <Text style={{ color: "gray", fontWeight: "600" }}>
+                  Video and sound quality: {item.videoQuality}
+                </Text>
+                <Text style={{ fontWeight: "600" }}>
+                  Resolution: {item.resolution}
+                </Text>
+              </View>
+
+              <MaterialCommunityIcons name="netflix" size={26} color="black" />
+            </View>
+
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+                marginTop: 16,
+              }}
+            >
+              <Text>Supported devices</Text>
+              <View
+                style={{ flexDirection: "row", alignItems: "center", gap: 5 }}
+              >
+                {item.devices.map((device) => (
+                  <Entypo name={device.name} size={26} color="#e50914" />
+                ))}
+              </View>
+            </View>
+
+            <View
+              style={{
+                marginTop: 15,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ width: "65%" }}>
+                Devices your household can watch at the same time
+              </Text>
+
+              <Text>{item.numberOfDevices}</Text>
+            </View>
+            <View
+              style={{
+                marginTop: 15,
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text>Download devices</Text>
+              <Text>{item.downloadDevices}</Text>
+            </View>
+          </Pressable>
+        ))}
+      </View>
+    </ScrollView>
+  );
+};
+
+export default Plans;
+
+const styles = StyleSheet.create({});
